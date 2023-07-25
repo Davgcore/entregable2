8ca57b3c27bd67e99a97da80543294fe554d2954
Davgcore <102384610+Davgcore@users.noreply.github.com>
2023-07-25 18:49:57 -0500

    email fixed

--- src/models/User.js
+++ src/models/User.js
@@ -11,7 +11,7 @@ const User = sequelize.define('user', {
         type: DataTypes.STRING,
         allowNull: false
     },
-    emial: {
+    email: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true
